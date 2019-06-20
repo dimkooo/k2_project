74b327ac025505bbdaf7a12ae4bca741d1ffc611
Viktor Kurysh <vik8174@gmail.com>
2019-06-20 11:25:08 +0300

    feat: Добавити header для navbar

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,23 @@
-const backdrop = document.getElementById('backdrop');
+///////////////////////
+// SIDEBAR
 
+const backdrop = document.getElementById('backdrop');
+const panel = document.getElementById('panel');
+
+// встановити ширину sidebar
+var bodyWidth = document.getElementsByTagName('body')[0].offsetWidth;
+bodyWidth = bodyWidth <= 360 ? bodyWidth : 360; 
+panel.style.width = bodyWidth + "px";
+panel.setAttribute('style',`width:${bodyWidth}px`);
+
+// показати sidebar
 const onShowSidebar = () => {
   backdrop.classList.add('backdrop--shown');
+  panel.classList.add('panel--shown');
 }
 
+// сховати sidebar
 const onHideSidebar = () => {
   backdrop.classList.remove('backdrop--shown');
+  panel.classList.remove('panel--shown');
 }
